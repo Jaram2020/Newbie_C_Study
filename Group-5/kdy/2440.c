d49bac225a466198fd0bd55e32faecb181c7c17e
kimdaye77 <kimdaye77@naver.com>
2020-04-10 21:23:38 +0900

    Create 2440.c file

---
+++ Group-5/kdy/2440.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+
+{
+  int N;
+
+
+  scanf("%d",&N);
+  int i=N;
+  while(i!=0){
+  
+    for (int j=i;j!=0;j--){
+      printf("*");
+    }
+    printf("\n");
+    i--;
+  }
+  
+    
+
+  return 0;
+}

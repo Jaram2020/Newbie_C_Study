8fb116ec559d5d41c39be289d3efe938b9b8cc70
devocta <ysjeong15@gmail.com>
2020-04-10 21:05:47 +0900

    Create 9498.c file

---
+++ Group-5/jys/9498.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    scanf("%d", &num);
+    if(num>=90){
+        printf("A");
+    }else if(num>=80){
+        printf("B");
+    }else if(num>=70){
+        printf("C");
+    }else if(num>=60){
+        printf("D");
+    }else{
+        printf("F");
+    }
+    
+    return 0;
+}
